5c0a6d7ca4ffdf88e7dc9a29fd4babcb3da027fe
nijikos <nijikos@gmail.com>
2026-02-21 19:26:01 +0700

    Revert "new logo"
    This reverts commit 6d55f7641adb8e4bf8cf3e5b9cf0effd4c9cd749.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,7 @@
 		{ label: 'products', href: '/' }
 	];
 
-	const main = {
+	const content = {
 		issues: [
 			{
 				icon: Package,
@@ -79,21 +79,6 @@
 				title: 'ANALYTICS',
 				icon: ChartLine
 			}
-		]
-	};
-
-	const footer = {
-		contact: {
-			WhatsApp: '0812 3456 7890',
-			Base: 'Jakarta, Indonesia'
-		},
-		links: [
-			{ label: 'About Us', href: '/' },
-			{ label: 'Lead Generation', href: '/' },
-			{ label: 'Outbound Sales Message', href: '/' },
-			{ label: 'Lead Tracker', href: '/' },
-			{ label: 'Project Management', href: '/' },
-			{ label: 'Inbound AI Chat Agent', href: '/' }
 		]
 	};
 </script>
@@ -213,25 +198,3 @@
 		</div>
 	</div>
 </header>
-<main></main>
-<!-- ---------- FOOTER -->
-<footer
-	class="absolute right-0 bottom-0 left-0 flex flex-col gap-8 bg-primary-800 px-8 py-14 text-white md:flex-row md:items-start md:justify-between"
->
-	<div class="">
-		<div class="mb-8">
-			<p class="text-2xl font-semibold uppercase">contact us</p>
-			<p class="">WHATSAPP : 0812 3456 7890</p>
-			<p class="">BASE : Jakarta, Indonesia</p>
-		</div>
-		<div class=""></div>
-	</div>
-	<div class="flex flex-col gap-4 md:items-end">
-		<img src="/images/logo/staumata-logo-white.webp" alt="logo-icon-bg" class="w-36" />
-		{#each footer.links as link}
-			<a href={link.href} class="underline-offset-4 hover:underline active:underline"
-				>{link.label}</a
-			>
-		{/each}
-	</div>
-</footer>
