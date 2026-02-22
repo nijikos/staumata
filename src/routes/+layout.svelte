53ad6042e5e20dc929619261069fab7825eb103a
nijikos <nijikos@gmail.com>
2026-02-22 12:01:04 +0700

    set favicon and title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
 	import './layout.css';
 	import '$lib/styles/scrollbar.scss';
-	import favicon from '$lib/assets/favicon.svg';
+	import favicon from '$lib/assets/favicon.ico';
 
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Staumata</title>
+</svelte:head>
 {@render children()}
